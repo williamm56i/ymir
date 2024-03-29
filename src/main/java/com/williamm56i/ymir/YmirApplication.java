bde5906fd4253c71b1b1067e95c16faa31ee9d38
Tony <wutony791225@hotmail.com>
2024-03-29 14:50:06 +0800

    swagger

--- src/main/java/com/williamm56i/ymir/YmirApplication.java
+++ src/main/java/com/williamm56i/ymir/YmirApplication.java
@@ -1,8 +1,10 @@
 package com.williamm56i.ymir;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition
 @SpringBootApplication
 public class YmirApplication {
 
--- src/main/java/com/williamm56i/ymir/controller/SystemController.java
+++ src/main/java/com/williamm56i/ymir/controller/SystemController.java
@@ -2,12 +2,15 @@ package com.williamm56i.ymir.controller;
 
 import com.williamm56i.ymir.persistence.vo.SysCode;
 import com.williamm56i.ymir.service.SystemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "系統")
 @RestController
 @RequestMapping("/api/system")
 public class SystemController {
@@ -15,11 +18,13 @@ public class SystemController {
     @Autowired
     SystemService systemService;
 
+    @Operation(summary = "GET測試")
     @GetMapping("/test")
     public ResponseEntity<String> getTest(@RequestParam("name") String name) {
         return ResponseEntity.ok(systemService.getTest(name));
     }
 
+    @Operation(summary = "取得系統參數")
     @GetMapping("/getSysCodeAll")
     public ResponseEntity<List<SysCode>> getSysCodeAll() {
         return ResponseEntity.ok(systemService.getSysCodeAll());
