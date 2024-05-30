c91ef725b0e3e4c8ec98efa430f1e4b4f659ea31
Tony <wutony791225@hotmail.com>
2024-05-30 10:04:09 +0800

    Global ExceptionHandler

---
+++ src/main/java/com/williamm56i/ymir/handler/YmirExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.williamm56i.ymir.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@Slf4j
+public class YmirExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ResponseEntity<String> exceptionHandler(Exception e) {
+        log.error("ExceptionHandler: {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
