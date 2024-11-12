39fe3af985d5ba3b0c99ce46c042365bea9bb9fe
williamm56i <wutony791225@hotmail.com>
2024-11-12 13:59:54 +0800

    exceptionHandler

--- src/main/java/com/williamm56i/ymir/handler/YmirExceptionHandler.java
+++ src/main/java/com/williamm56i/ymir/handler/YmirExceptionHandler.java
@@ -3,9 +3,11 @@ package com.williamm56i.ymir.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 @Slf4j
@@ -17,5 +19,26 @@ public class YmirExceptionHandler {
         e.printStackTrace();
         log.error("ExceptionHandler: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = ResponseStatusException.class)
+    @ResponseBody
+    public ResponseEntity<String> responseStatusExceptionHandler(Exception e) {
+        e.printStackTrace();
+        log.error("responseStatusExceptionHandler: {}", e.getMessage());
+        if (HttpStatus.UNAUTHORIZED.toString().equals(e.getMessage())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        } else if (HttpStatus.FORBIDDEN.toString().equals(e.getMessage())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = AuthorizationDeniedException.class)
+    @ResponseBody
+    public ResponseEntity<String> authorizationDeniedExceptionHandler(Exception e) {
+        e.printStackTrace();
+        log.error("authorizationDeniedExceptionHandler: {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
 }
